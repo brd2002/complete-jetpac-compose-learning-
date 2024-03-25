85debe6d6ece610f04775b2125a83fc593ef105a
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-25 18:31:44 +0530

    this is my first commit

---
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -0,0 +1,41 @@
+package com.example.blogapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.blogapp.ui.theme.BlogAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            BlogAppTheme {
+                // A surface container using the 'background' color from the theme
+               Text(text = "welcome to my app" , style = MaterialTheme.typography.headlineLarge)
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    BlogAppTheme {
+        Greeting("Android")
+    }
+}
