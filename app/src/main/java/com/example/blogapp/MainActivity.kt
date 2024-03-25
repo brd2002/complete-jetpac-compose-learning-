7e9bd3985387f2778db8710e3ec738acef6bb474
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-25 20:48:36 +0530

    lecture 12 completed

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.blogapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.blogapp.ui.theme.BlogAppTheme
@@ -16,26 +21,22 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            BlogAppTheme {
-                // A surface container using the 'background' color from the theme
-               Text(text = "welcome to my app" , style = MaterialTheme.typography.headlineLarge)
-            }
+            App()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    BlogAppTheme {
-        Greeting("Android")
+fun App(){
+    var theme = remember {
+        mutableStateOf(false)
+    }
+    BlogAppTheme(theme.value) {
+        Column(Modifier.background(MaterialTheme.colorScheme.background)) {
+            Text(text = "welcome to my app" , style = MaterialTheme.typography.headlineLarge)
+            Button(onClick = { theme.value = !theme.value }) {
+                Text(text = "Change theme ")
+            }
+        }
     }
 }
