c31531e40363cffaf799d7a84f4389f78e9919c1
Bharat Ruidas <74824417+brd2002@users.noreply.github.com>
2024-03-25 21:22:35 +0530

    lecture 13 completed

--- app/src/main/java/com/example/blogapp/MainActivity.kt
+++ app/src/main/java/com/example/blogapp/MainActivity.kt
@@ -1,19 +1,24 @@
 package com.example.blogapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.blogapp.ui.theme.BlogAppTheme
 
@@ -21,22 +26,59 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            App()
+//            App()
+//            ListComposable()
+            Counter()
         }
     }
 }
-
 @Composable
 fun App(){
     var theme = remember {
         mutableStateOf(false)
     }
     BlogAppTheme(theme.value) {
-        Column(Modifier.background(MaterialTheme.colorScheme.background)) {
+        Column() {
             Text(text = "welcome to my app" , style = MaterialTheme.typography.headlineLarge)
             Button(onClick = { theme.value = !theme.value }) {
                 Text(text = "Change theme ")
             }
         }
+    }
+}
+
+@Preview(showBackground = true , showSystemUi = true , name = "testing screen")
+@Composable
+fun ListComposable() {
+    val alllist = remember {
+        mutableStateOf(emptyList<String>())
+    }
+    LaunchedEffect(key1 = Unit) {
+        alllist.value = fetchdata()
+    }
+
+    LazyColumn {
+        items(alllist.value){
+            Text(text = it)
+        }
+    }
+}
+
+fun fetchdata() : List<String>{
+    return listOf("bharat" , "home" , "kisan" , "binoy" ) 
+}
+
+@Composable
+fun Counter() {
+    val count = remember {
+        mutableStateOf(0)
+    }
+    var key = count.value % 3 == 0
+    LaunchedEffect(key1 = key) {
+        Log.d("count", "Counter: value is now ${count.value}")
+    }
+
+    Button(onClick = {count.value++}) {
+        Text(text = "Increment the value of count")
     }
 }
